8618ca169a495d5b1075f9e71fd9600e1f326a9c
hafizrahmadhani <mhafiz.rahmadhani@gmail.com>
2021-10-08 00:38:17 +0700

    add intrinsics material

--- app/src/main/java/com/hafizrahmadhani/constraintlayout/MainActivity.kt
+++ app/src/main/java/com/hafizrahmadhani/constraintlayout/MainActivity.kt
@@ -3,19 +3,15 @@ package com.hafizrahmadhani.constraintlayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
 import com.hafizrahmadhani.constraintlayout.ui.theme.ConstraintLayoutTheme
 
@@ -26,7 +22,7 @@ class MainActivity : ComponentActivity() {
             ConstraintLayoutTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    LargeConstraintLayout()
+                    TwoTexts(text1 = "Hi", text2 = "there")
                 }
             }
         }
@@ -86,7 +82,7 @@ fun LargeConstraintLayout() {
 @Preview
 @Composable
 fun ConstraintLayoutContentPreview() {
-    ConstraintLayoutTheme() {
+    ConstraintLayoutTheme {
         ConstraintLayoutContent()
     }
 }
@@ -94,7 +90,42 @@ fun ConstraintLayoutContentPreview() {
 @Preview
 @Composable
 fun LargeConstraintLayoutPreview() {
-    ConstraintLayoutTheme() {
+    ConstraintLayoutTheme {
         LargeConstraintLayout()
+    }
+}
+
+//INTRINSICS MATERIAL
+@Composable
+fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
+    Row(modifier = modifier.height(IntrinsicSize.Min)) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+                .wrapContentWidth(Alignment.Start),
+            text = text1
+        )
+
+        Divider(color = Color.Black, modifier = Modifier
+            .fillMaxHeight()
+            .width(1.dp))
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 4.dp)
+                .wrapContentWidth(Alignment.End),
+            text = text2
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TwoTextsPreview() {
+    ConstraintLayoutTheme {
+        Surface {
+            TwoTexts(text1 = "Hi", text2 = "there")
+        }
     }
 }
